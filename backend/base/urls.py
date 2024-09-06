c38282d9ac9a3f64b5389435478c3ada8ddd9868
Antonio Furtado <antoniordf@hotmail.com>
2024-09-06 22:36:56 +0100

    Transformed my views to class based views

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import real_gdp_metadata
+from .views import RealGDPMetadataView, RealGDPDataPointsView
 
 app_name = 'base'
 
 urlpatterns = [
-    path('real_gdp_metadata/', real_gdp_metadata, name='real_gdp_metadata'),
+    path('real_gdp_metadata/', RealGDPMetadataView.as_view(), name='real_gdp_metadata'),
+    path('real_gdp_data_points/<str:series_id>/', RealGDPDataPointsView.as_view(), name='real_gdp_data_points'),
 ]
--- backend/base/views/real_gdp_views.py
+++ backend/base/views/real_gdp_views.py
@@ -1,16 +1,24 @@
 from django.http import JsonResponse
+from django.views import View
 from base.models import RealGDP
 
-def real_gdp_metadata(request):
-    real_gdp_metadata = RealGDP.objects.all()
-    data = list(real_gdp_metadata.values())
-    return JsonResponse(data, safe=False)
-    
+# Class-based view for Real GDP metadata
+class RealGDPMetadataView(View):
+    def get(self, request):
+        real_gdp_metadata = RealGDP.objects.all()
+        data = list(real_gdp_metadata.values())
+        return JsonResponse(data, safe=False)
 
-def real_gdp_data_points(request, series_id):
-    # Retrieve the specific Real GDP series by series_id
-    real_gdp_series = RealGDP.objects.get(series_id=series_id)
-    # Retrieve the associated data points, ordered by date as per the Meta class in DataPoint
-    data_points = real_gdp_series.data_points.all()
-    data = list(data_points.values())
-    return JsonResponse(data, safe=False)
+
+# Class-based view for Real GDP data points by series_id
+class RealGDPDataPointsView(View):
+    def get(self, request, series_id):
+        try:
+            # Retrieve the specific Real GDP series by series_id
+            real_gdp_series = RealGDP.objects.get(series_id=series_id)
+            # Retrieve the associated data points, ordered by date as per the Meta class in DataPoint
+            data_points = real_gdp_series.data_points.all()
+            data = list(data_points.values())
+            return JsonResponse(data, safe=False)
+        except RealGDP.DoesNotExist:
+            return JsonResponse({'error': 'RealGDP series not found'}, status=404)
