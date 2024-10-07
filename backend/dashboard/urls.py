055db4887c4914de9ca7f4120da84cbede3ea95c
Antonio Furtado <antoniordf@hotmail.com>
2024-10-07 22:43:42 +0100

    adding additional views and paths for other indicators

--- backend/dashboard/urls.py
+++ backend/dashboard/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import RealGDPMetadataView, RealGDPDataPointsView, CorporateBondSpreadMetadataView, CorporateBondSpreadDataPointsView, FedFundsRateMetadataView, FedFundsRateDataPointsView
+from .views import RealGDPMetadataView, RealGDPDataPointsView, CorporateBondSpreadMetadataView, CorporateBondSpreadDataPointsView
+from .views import FedFundsRateMetadataView, FedFundsRateDataPointsView, NominalInflationMetadataView, NominalInflationDataPointsView
 
 app_name = 'dashboard'
 
@@ -10,4 +11,6 @@ urlpatterns = [
     path('corporate_bond_spread_data_points/<str:series_id>/', CorporateBondSpreadDataPointsView.as_view(), name='corporate_bond_spread_data_points'),
     path('fed_funds_rate_metadata/', FedFundsRateMetadataView.as_view(), name='fed_funds_rate_metadata'),
     path('fed_funds_rate_data_points/<str:series_id>/', FedFundsRateDataPointsView.as_view(), name='fed_funds_rate_data_points'),
+    path('nominal_inflation_metadata/', NominalInflationMetadataView.as_view(), name='nominal_inflation_metadata'),
+    path('nominal_inflation_data_points/<str:series_id>/', NominalInflationDataPointsView.as_view(), name='nominal_inflation_data_points'),
 ]
